45c87eee65c79ba948adc859b4677e0556e7aa56
amandafadilaerros <amandafadila1@gmail.com>
2024-09-27 07:06:18 +0700

    pertemuan3

---
+++ Pertemuan3/praktikum1.dart
@@ -0,0 +1,26 @@
+void main() {
+  String test = "test2";
+
+  if (test == "test1") {
+    print("Test1");
+  } else if (test == "test2") {
+    print("Test2");
+  } else {
+    print("Something else");
+  }
+
+  if (test == "test2") {
+    print("Test2 again");
+  }
+}
+
+// PERTANYAAN
+// Perbandingan String di Dart:
+
+// Di Dart, perbandingan string dengan operator == berfungsi sebagaimana mestinya. Tidak seperti Java, Dart membandingkan nilai string dengan benar menggunakan ==.
+// Penggunaan else if dan else:
+
+// Penulisan kondisi di Dart menggunakan else if (huruf kecil), sesuai dengan aturan bahasa Dart.
+// Penggunaan print:
+
+// Di Dart, untuk mencetak output ke konsol, cukup gunakan print().
---
+++ Pertemuan3/praktikum2.dart
@@ -0,0 +1,7 @@
+void main() {
+  int counter = 0;
+  while (counter < 33) {
+    print(counter);
+    counter++;
+  }
+}
